dc8d311431b11536dbf5837aa4d21ffae6c9d7cd
Tanner Hill <tanner.hill@localeyes.guide>
2023-01-25 21:33:49 -0700

    This one did not add :/

---
+++ Unit1B/Unit1B_Lab_Conditionals_Practice1.cs
@@ -0,0 +1,22 @@
+using System;
+public class greater_than
+{
+	public void Main()
+	{
+	int x = 10;
+	int y = 10;
+	if (x > y)
+		{
+  		Console.WriteLine("Hello World");
+		} else if (x != y)
+	{
+		Console.WriteLine("X does not equal Y");
+	} else if (x == y)
+	{
+		Console.WriteLine("X does equal Y");
+	} else
+	{
+		Console.WriteLine("X is less than Y");
+	}
+}
+}
