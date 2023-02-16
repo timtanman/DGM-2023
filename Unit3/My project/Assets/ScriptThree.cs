1b0d09acab01feff6dfe8b151731fe7c57f30378
Tanner Hill <tanner.hill@localeyes.guide>
2023-02-15 19:30:09 -0700

    Adjusted the code a bit

--- Unit3/My project/Assets/ScriptThree.cs
+++ Unit3/My project/Assets/ScriptThree.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class ScriptThree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Start ()
     {
         Debug.Log(transform.position.x);
-
+        
         if(transform.position.y <= 5f)
         {
-            Debug.Log("I'm about to hit the ground! And probably will die soon after...");
+            Debug.Log ("I'm about to hit the ground and will probably die...");
         }
     }
-
 }
--- Unit3/My project/Assets/ScriptTwo.cs
+++ Unit3/My project/Assets/ScriptTwo.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ScriptTwo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int myInt = 5;
+
     void Start()
     {
         myInt = MultiplyByTwo(myInt);
