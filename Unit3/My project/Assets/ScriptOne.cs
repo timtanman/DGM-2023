6940e9746aa0b3b83b305b6c483c275b2334c638
Tanner Hill <tanner.hill@localeyes.guide>
2023-02-15 19:02:13 -0700

    Adding ScriptOne

---
+++ Unit3/My project/Assets/ScriptOne.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+public class ScriptOne : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer> ().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer> ().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer> ().material.color = Color.blue;
+        }
+    }
+}
