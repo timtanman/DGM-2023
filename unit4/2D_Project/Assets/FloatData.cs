37550cd7927c8d5b6c48ad843668c1c432fb5789
Tanner Hill <tanner.hill@localeyes.guide>
2023-02-23 15:46:45 -0700

    Added two new powerups

--- unit4/2D_Project/Assets/FloatData.cs
+++ unit4/2D_Project/Assets/FloatData.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class FloatData : ScriptableObject
 {
     public float value;
+    public float power;
+    public float reset;
 
     public void UpdateValue(float num)
     {
         value += num;
     }
+
+        public void PowerUpdate(float num)
+    {
+        power += num;
+    }
+
+    public void ResetUpdate(float num)
+    {
+        reset = 10;
+        power = 10;
+        value = 10;
+    }
+
 }
--- unit4/2D_Project/Assets/TextLabelBehavior.cs
+++ unit4/2D_Project/Assets/TextLabelBehavior.cs
@@ -7,16 +7,24 @@ public class TextLabelBehavior : MonoBehaviour
 {
     public Text label;
     public FloatData dataObj;
+    
 
     public void Start()
     {
         label = GetComponent<Text>();
         UpdateLabel();
+        UpdateLabelPower();
     }
 
     public void UpdateLabel()
     {
         label.text = dataObj.value.ToString();
     }
+
+        public void UpdateLabelPower()
+    {
+        label.text = dataObj.power.ToString();
+    }
+
 
 }
